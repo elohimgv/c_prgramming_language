782e749252bea2a755b84dcf4ff0eebd476fa17d
elohim <elohim.guevara@gmail.com>
2023-10-03 10:40:16 -0600

    Add Exercise 1-2

---
+++ chapter_1/ex1-2/fharenheit_to_celsius.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* Print fharenheit to Celsius table 
+    for fhar = 0, 20, ... 300 */
+
+int main() 
+{
+    float fhar, celsius;
+    int lower, upper, step;
+
+    lower = 0; /* lower limit of temperature table */
+    upper = 300; /* uppper limit */
+    step = 20; /* step size */
+
+    fhar = lower;
+    while (fhar <= upper) {
+        celsius = (5.0/9.0) * (fhar-32.0) ; /* formula */
+        printf("%3.0f\t%6.1f\n", fhar, celsius);
+        fhar = fhar + step;
+    }
+}
